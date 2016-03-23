b3016b0919d02c14aa00a899a778d356a916d34c
jos <wjosdejong@gmail.com>
2016-03-23 13:17:51 +0100

    Set up SPA with two panels

--- src/App.js
+++ src/App.js
@@ -1,11 +1,72 @@
 import React, { Component } from 'react';
+import debugFactory from 'debug/browser';
+
+import AppBar from 'material-ui/lib/app-bar';
+import LeftNav from 'material-ui/lib/left-nav';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+
+import InputForm from './InputForm';
+import ProfitAndLoss from './ProfitAndLoss';
+
+const debug = debugFactory('vbi:app');
+
+// Road map:
+// - create two panels: left panel for input, right panel for profit & loss
+// - left panel shows excel-like tables
+// - the right panel shows a profit&loss table
+// - later on we could create a business model canvas input format in the left panel
+// - create an option to show the numbers as a cumulative bar chart
+
+
+// expose the debug library to window, so we can enable and disable it
+if (typeof window !== 'undefined') {
+  console.info('To enable debugging, enter debug.enable(\'vbi:*\') in this console, then refresh the page');
+  console.info('To disable debugging, enter debug.disable() in this console, then refresh the page');
+  window.debug = debugFactory;
+}
+
+
+var data = require('../data/example_company.json');
+
+debug('data', data);
+
 
 export default class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      showLeftNav: false
+    };
+  }
+
   render() {
     return (
       <div>
-        <h1>VBI</h1>
-        <p>An interactive business intelligence tool</p>
+        <AppBar
+            style={{position: 'fixed', top: 0, left: 0, zIndex: 999, background: 'darkorange'}}
+            title="VanPaz Business Intelligence"
+        />
+
+        <LeftNav docked={false}
+                 width={200}
+                 open={this.state.showLeftNav}
+                 onRequestChange={showLeftNav => this.setState({showLeftNav})} >
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </LeftNav>
+
+        <div className="container">
+          <InputForm />
+        </div>
+
+        <div className="container">
+          <ProfitAndLoss />
+        </div>
+
+        <div className="footer">
+          Copyright &copy; <a href="http://vanpaz.com">VanPaz</a> 2016
+        </div>
       </div>
     );
   }
---
+++ src/ProfitAndLoss.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import debugFactory from 'debug/browser';
+
+import Card from 'material-ui/lib/card/card';
+import CardTitle from 'material-ui/lib/card/card-title';
+import CardText from 'material-ui/lib/card/card-text';
+
+const debug = debugFactory('vbi:profit-loss');
+
+export default class ProfitAndLoss extends Component {
+  render () {
+    return <Card className="card">
+      <CardTitle title="Profit and loss" subtitle="Show profit and loss based on provided costs and revenues" />
+      <CardText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+        Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+        Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+      </CardText>
+    </Card>;
+
+  }
+}
